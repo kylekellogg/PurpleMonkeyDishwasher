ecd25a2467124b3b56e6de739a8eef38adfc49ae
Dan Barron <stereoalliance@gmail.com>
2013-11-09 16:24:49 -0500

    extract game object to own file

---
+++ client/game.js
@@ -0,0 +1,34 @@
+if (typeof window.Game === "undefined") {
+  window.Game = {};
+}
+
+Game.Core = function() {
+  this.initialize.apply(this, arguments)
+};
+
+(function(proto){
+  proto.initialize = function() {
+    console.log("Starting the game")
+  }
+
+  proto.ready = function() {
+    var Canvas = document.getElementById("game")
+    var Context = Canvas.getContext("2d")
+
+    var viewportWidth = window.innerWidth
+    var viewportHeight = window.innerHeight
+    var canvasWidth = Canvas.getAttribute("width")
+    var canvasHeight = Canvas.getAttribute("height")
+
+    Canvas.style.position = "absolute"
+    Canvas.style.top = (viewportHeight - canvasHeight) / 2 + "px"
+    Canvas.style.left = (viewportWidth - canvasWidth) / 2 + "px"
+
+    var floorImg = new Image()
+    floorImg.src = "images/floor.jpg"
+    floorImg.addEventListener("load", function() {
+      Context.drawImage(floorImg, 0, 0)
+    })
+  }
+})(Game.Core.prototype);
+
--- client/main.js
+++ client/main.js
@@ -15,38 +15,6 @@ Meteor.call( 'requestPlayerID', function( e, r ) {
 	console.log( 'playerID:', playerID );
 } );
 
-if (typeof window.Game === "undefined") {
-  window.Game = function() {
-    this.initialize.apply(this, arguments)
-  }
-}
+var theGame = new Game.Core();
 
-(function(proto){
-  proto.initialize = function() {
-    console.log("Starting the game")
-  }
-
-  proto.ready = function() {
-    var Canvas = document.getElementById("game")
-    var Context = Canvas.getContext("2d")
-
-    var viewportWidth = window.innerWidth
-    var viewportHeight = window.innerHeight
-    var canvasWidth = Canvas.getAttribute("width")
-    var canvasHeight = Canvas.getAttribute("height")
-
-    Canvas.style.position = "absolute"
-    Canvas.style.top = (viewportHeight - canvasHeight) / 2 + "px"
-    Canvas.style.left = (viewportWidth - canvasWidth) / 2 + "px"
-
-    var floorImg = new Image()
-    floorImg.src = "images/floor.jpg"
-    floorImg.addEventListener("load", function() {
-      Context.drawImage(floorImg, 0, 0)
-    })
-  }
-})(Game.prototype)
-
-var theGame = new Game()
-
-window.onload = theGame.ready
+window.onload = theGame.ready;
