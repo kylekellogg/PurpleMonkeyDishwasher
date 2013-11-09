f49745dc710e4a677fd8fbf695652bc56747c989
Kyle Kellogg <kyle@kylekellogg.com>
2013-11-09 16:12:33 -0500

    Updates to merge clients/main.js manually

--- client/main.js
+++ client/main.js
@@ -1,7 +1,36 @@
 var playerID;
 
-window.onload = function() {
-	Meteor.call( 'requestPlayerID', function( e, r ) {
+if (typeof window.Game === "undefined") {
+  window.Game = function() {
+    this.initialize.apply(this, arguments)
+  }
+}
+
+(function(proto){
+  proto.initialize = function() {
+    console.log("Starting the game")
+  }
+
+  proto.ready = function() {
+    var Canvas = document.getElementById("game")
+    var Context = Canvas.getContext("2d")
+
+    var viewportWidth = window.innerWidth
+    var viewportHeight = window.innerHeight
+    var canvasWidth = Canvas.getAttribute("width")
+    var canvasHeight = Canvas.getAttribute("height")
+
+    Canvas.style.position = "absolute"
+    Canvas.style.top = (viewportHeight - canvasHeight) / 2 + "px"
+    Canvas.style.left = (viewportWidth - canvasWidth) / 2 + "px"
+
+    var floorImg = new Image()
+    floorImg.src = "images/floor.jpg"
+    floorImg.addEventListener("load", function() {
+      Context.drawImage(floorImg, 0, 0)
+    })
+
+    Meteor.call( 'requestPlayerID', function( e, r ) {
 		if ( e ) {
 			//	TODO: Handle error
 			console.log( 'Error getting playerID' );
@@ -15,7 +44,12 @@ window.onload = function() {
 		}
 		console.log( 'playerID:', playerID );
 	} );
-};
+  }
+})(Game.prototype)
+
+var theGame = new Game()
+
+window.onload = theGame.ready
 
 window.onbeforeunload = function() {
 	Meteor.call( 'returnPlayerID', playerID );
