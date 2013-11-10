4a2a5e238cd271d433a4858d7c3d96f450de601e
Kyle Kellogg <kyle@kylekellogg.com>
2013-11-10 11:50:11 -0500

    Introduced requestAnimFrame shim

--- client/main.js
+++ client/main.js
@@ -7,6 +7,15 @@ var players = {},
 	iid,
 	bg;
 
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
 function Game() {
 	this.players = [];
 	this.secrets = [];
@@ -102,7 +111,10 @@ function ready(e) {
     	}
     } );
 
-    iid = setInterval( update, 1/30 );
+    (function animLoop() {
+    	requestAnimFrame( animLoop );
+    	update();
+    })();
   } );
 }
 
